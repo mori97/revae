c24eee527d932d047242c5a04f0f2962d0acf1d5
Yicheng DU <du@sap.ist.i.kyoto-u.ac.jp>
2020-08-03 18:27:20 +0900

    Add reparameterize function

---
+++ models/common.py
@@ -0,0 +1,11 @@
+import torch
+
+
+@torch.jit.script
+def reparameterize(mu, logvar):
+    """Reparameterization trick.
+    """
+    std = torch.exp(0.5 * logvar)
+    eps = torch.randn_like(std)
+    z = mu + eps * std
+    return z
