b94df1aa058ce6d7d768fa5f313cc5ad78706e1f
Yicheng DU <du@sap.ist.i.kyoto-u.ac.jp>
2020-08-03 19:54:38 +0900

    Add get_fashion_mnist

--- datasets.py
+++ datasets.py
@@ -1,4 +1,5 @@
 from itertools import chain
+import random
 
 import torch
 from torchvision import datasets, transforms
@@ -27,5 +28,29 @@ def get_celeba():
     # Use 'train' split and 'valid' split as the training set
     train = [(img, label[mask]) for img, label in chain(train, valid)]
     test = [(img, label[mask]) for img, label in test]
+
+    return train, test
+
+
+def get_fashion_mnist(supervision_rate=0.06, seed=0):
+    """Get and preprocess the Fashion-MNIST dataset.
+    """
+    random.seed(seed)
+
+    train = datasets.FashionMNIST('./data', train=True, download=True,
+                                  transform=transforms.ToTensor())
+    test = datasets.FashionMNIST('./data', train=False, download=True,
+                                 transform=transforms.ToTensor())
+
+    # Mask some labels as unsupervised data
+    for t in range(10):
+        mask = (train.targets == t)
+        ts = train.targets[mask]
+        uns = [True if i >= ts.size(0) * supervision_rate else False
+               for i in range(ts.size(0))]
+        random.shuffle(uns)
+        uns = torch.tensor(uns)
+        mask[mask] = uns
+        train.targets[mask] = -1
 
     return train, test
