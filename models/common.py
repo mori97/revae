f4e54f51f9cfc3874b6055dab5a37bd4eda26660
Yicheng DU <du@sap.ist.i.kyoto-u.ac.jp>
2020-08-04 16:39:53 +0900

    Modify reparameterize function
    Add n_samples argument and add docs.

--- models/common.py
+++ models/common.py
@@ -1,11 +1,20 @@
 import torch
 
 
-@torch.jit.script
-def reparameterize(mu, logvar):
+def reparameterize(mu, logvar, n_samples=1):
     """Reparameterization trick.
+
+    Args:
+        mu (torch.Tensor): Mean.
+        logvar (torch.Tensor): Logarithm of variation.
+        n_samples (int): The number of samples.
+
+    Returns:
+        torch.Tensor: Samples drawn from the given Gaussian distribution.
+            The shape is equal to mu if n_samples is 1,
+            and (n_samples, *mu.shape) if n_samples is larger than 1.
     """
     std = torch.exp(0.5 * logvar)
-    eps = torch.randn_like(std)
+    eps = torch.randn(n_samples, *std.size())
     z = mu + eps * std
-    return z
+    return z.squeeze(0)
