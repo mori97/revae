c55f4c9001eff344e485841902b0896b59921eff
Yicheng DU <du@sap.ist.i.kyoto-u.ac.jp>
2020-08-05 14:28:11 +0900

    Add REVAEMNIST
    At this time, only for supervised learning.

--- models/mnist.py
+++ models/mnist.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions.normal import Normal
+
+from .common import reparameterize
 
 
 class _Encoder(nn.Module):
@@ -61,3 +64,84 @@ class _ConditionalPrior(nn.Module):
         h = F.relu(self.fc1(y))
         mu, logvar = torch.chunk(self.fc2(h), 2, dim=1)
         return mu, logvar
+
+
+class REVAEMNIST(nn.Module):
+    """Reparameterized VAE for the MNIST dataset.
+
+    Args:
+        z_c_dim (int): The dimension of z_c.
+        z_exc_dim (int): The dimension of z_\c.
+    """
+    def __init__(self, z_c_dim, z_exc_dim):
+        super(REVAEMNIST, self).__init__()
+
+        self._z_c_dim = z_c_dim
+        self._z_exc_dim = z_exc_dim
+
+        self.encoder = _Encoder(z_c_dim + z_exc_dim)
+        self.decoder = _Decoder(z_c_dim + z_exc_dim)
+        self.classifier = _Classifier(z_c_dim)
+        self.cond_prior = _ConditionalPrior(z_c_dim)
+
+
+    @property
+    def z_c_dim(self):
+        return self._z_c_dim
+
+    @property
+    def z_exc_dim(self):
+        return self._z_exc_dim
+
+    def forward(self, x, t):
+        """Output the upper bound (the negative lower bound) of each sample for
+        optimization.
+
+        Args:
+            x (torch.Tensor): Samples of shape (N, 784).
+                where N is the batch size.
+            t (torch.Tensor): The labels of shape (N,).
+
+        Returns:
+            torch.Tensor : The negative lower bound of each sample.
+                The shape of returned value is (N,).
+        """
+        # TODO: Unsupervised case
+        eye = torch.eye(10, device=t.device)
+        batch_size = x.size(0)
+
+        z_mu, z_logvar = self.encoder(x)
+        z = reparameterize(z_mu, z_logvar)
+        z_c, z_exc = z[:, :self.z_c_dim], z[:, self.z_c_dim:]
+        recon = self.decoder(z)
+
+        # log p(x|z)
+        log_p_x_z = -F.binary_cross_entropy(recon, x, reduction='none').sum(1)
+        # log p(z_c|y)
+        z_c_mu, z_c_logvar = self.cond_prior(eye[t])
+        z_c_std = torch.exp(0.5 * z_c_logvar)
+        log_p_zc_y = Normal(z_c_mu, z_c_std).log_prob(z_c).sum(1)
+        # log p(z_\c)
+        dist = Normal(torch.zeros_like(z_exc), torch.ones_like(z_exc))
+        log_p_zexc = dist.log_prob(z_exc).sum(1)
+        # log p(z|y)
+        log_p_z_y = log_p_zc_y + log_p_zexc
+        # log q(y|z_c)
+        y = F.log_softmax(self.classifier(z_c), dim=1)
+        log_q_y_zc = -F.nll_loss(y, t, reduction='none')
+        # log q(y|x)  ( Draw 128 points from q(z_c|x) )
+        h = reparameterize(z_mu[:, :self.z_c_dim], z_logvar[:, :self.z_c_dim],
+                           n_samples=128)
+        h = self.classifier(h.reshape(128 * batch_size, h.size(2)))
+        h = F.softmax(h, dim=1).reshape(128, batch_size, h.size(1))
+        log_q_y_x = torch.mean(h, dim=0)[torch.arange(batch_size), t].log()
+        # log q(z|x)
+        z_std = torch.exp(0.5 * z_logvar)
+        log_q_z_x = Normal(z_mu, z_std).log_prob(z).sum(1)
+
+        # Calculate the lower bound
+        h = log_p_x_z + log_p_z_y - log_q_y_zc - log_q_z_x
+        coef = torch.exp(log_q_y_zc - log_q_y_x)
+        lb = coef * h + log_q_y_x
+
+        return -lb
