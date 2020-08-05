530d5cf861bb353e18a67825d5dd93f5e510a14a
Yicheng DU <du@sap.ist.i.kyoto-u.ac.jp>
2020-08-05 14:27:06 +0900

    Fix reparameterize function

--- models/common.py
+++ models/common.py
@@ -15,6 +15,6 @@ def reparameterize(mu, logvar, n_samples=1):
             and (n_samples, *mu.shape) if n_samples is larger than 1.
     """
     std = torch.exp(0.5 * logvar)
-    eps = torch.randn(n_samples, *std.size())
+    eps = torch.randn(n_samples, *std.size(), device=std.device)
     z = mu + eps * std
     return z.squeeze(0)
