1d613c8bbf16b310eb7cf49c206855d7d03e3f9c
Yicheng DU <du@sap.ist.i.kyoto-u.ac.jp>
2020-08-07 17:46:20 +0900

    Modify train.py for semi-supervised training

--- train.py
+++ train.py
@@ -90,13 +90,16 @@ def main():
     parser.add_argument('--log-dir',
                         help='TensorBoard save directory location.',
                         type=str, default=None)
+    parser.add_argument('--supervised-rate', '-s',
+                        help='Supervised rate.',
+                        type=float, default=0.2)
     args = parser.parse_args()
 
     if_use_cuda = torch.cuda.is_available() and args.gpu >= 0
     device = torch.device('cuda:{}'.format(args.gpu) if if_use_cuda else 'cpu')
 
     train_dataset, test_dataset = \
-        datasets.get_fashion_mnist(supervision_rate=1.0)
+        datasets.get_fashion_mnist(supervision_rate=0.2)
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=args.batch_size, shuffle=True,
         num_workers=2, pin_memory=if_use_cuda)
